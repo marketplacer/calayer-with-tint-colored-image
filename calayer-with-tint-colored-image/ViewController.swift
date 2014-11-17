7d203ae9d5645ffb8730d803b014f647652131d3
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-11-17 11:36:47 +1100

    Initial Commit

---
+++ calayer-with-tint-colored-image/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calayer-with-tint-colored-image
+//
+//  Created by Evgenii Neumerzhitckii on 17/11/2014.
+//  Copyright (c) 2014 The Exchange Group Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
